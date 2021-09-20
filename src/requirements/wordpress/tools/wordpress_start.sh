1e8d91b32665a5c3d01d433ddaf3d4c61e568d62
Mathos Likt <mlikt@ev-j4.kzn.21-school.ru>
2021-09-20 23:06:19 +0300

    Inception

--- src/requirements/wordpress/tools/wordpress_start.sh
+++ src/requirements/wordpress/tools/wordpress_start.sh
@@ -2,9 +2,10 @@
 
 service php7.3-fpm start
 
-# Подключение к базе данных
+# Скачеваем установочные файлы
 wp core download    --allow-root --path="/var/www/html"
 
+# Подключение к базе данных
 wp core config	--allow-root \
 				--skip-check \
 				--dbname=$DB_NAME \
@@ -14,6 +15,7 @@ wp core config	--allow-root \
 				--dbprefix=$DB_PREFIX \
 				--path="/var/www/html"
 
+# Устанавливаем и создаем администратора
 wp core install	--allow-root \
 				--url=$DOMAIN_NAME \
 				--title="ecole 21" \
@@ -21,6 +23,22 @@ wp core install	--allow-root \
 				--admin_password="mlikt" \
 				--admin_email="mlikt@student.21-school.ru" \
 				--path="/var/www/html"
+
+
+# Создаем еще 2 пользователей
+wp user create      tor tor@mlikt.42.com \
+                    --role=author \
+                    --user_pass="tor" \
+                    --allow-root \
+					--path="/var/www/html"
+
+wp user create      loki loki@mlikt.42.com \
+                    --role=author \
+                    --user_pass="loki" \
+                    --allow-root \
+					--path="/var/www/html"
+
+#  Без этих записей не подгружеат таблицу стилей и остальной контент
 
 sed "2idefine('WP_HOME','https://$DOMIAN_NAME');" /var/www/html/wp-config.php >> /var/www/html/wp-config.php.new
 mv /var/www/html/wp-config.php.new /var/www/html/wp-config.php
